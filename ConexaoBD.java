d26dfbe09455f8a51202c4ae19dd3d2c9746eba7
Joice :P <joicemarazanini@gmail.com>
2018-12-09 10:30:10 -0200

    Update ConexaoBD.java

--- ConexaoBD.java
+++ ConexaoBD.java
@@ -12,20 +12,21 @@ private Connection conexao;
 	public Connection conectar() {
 		try {
 		Class.forName("com.mysql.cj.jdbc.Driver");
-		conexao = DriverManager.getConnection("jdbc:mysql://esn509vmysql:3306/"+// no senai È: "jdbc:mysql://esn509vmysql:3306/"
-				"projetojoice"+"?user=aluno"+"&password=Senai1234"
+		conexao = DriverManager.getConnection("/*nome do endere√ßo*/"+
+				"/*nome do banco de dados do mysql que quer usar*/"+"?user=/*loguin para entrar no mysql*/"+
+						      "&password=/*senha pra entrar no mysql*/"
 				+"&useSSL=false&useTimezone=true&serverTimezone=UTC");
 		System.out.println(" Conectou :) ao banco de dados");
 		
 		
 	}
 	catch(ClassNotFoundException e) {
-		System.out.println("Driver n„o encontrado! "+"\n"+"VocÍ N√O EST¡ EM CONEX√O COM O BANCO DE DADOS"+e.toString());
-		//JOptionPane.showMessageDialog(null, "erro - Driver n„o encontrado "+"\n"+"VocÍ N√O EST¡ EM CONEX√O COM O BANCO DE DADOS"+e.toString());
+		System.out.println("Driver n√£o encontrado! "+"\n"+"Voc√™ N√ÉO EST√Å EM CONEX√ÉO COM O BANCO DE DADOS"+e.toString());
+		//JOptionPane.showMessageDialog(null, "erro - Driver n√£o encontrado "+"\n"+"Voc√™ N√ÉO EST√Å EM CONEX√ÉO COM O BANCO DE DADOS"+e.toString());
 		e.printStackTrace();
 	}catch(SQLException e) {
-		System.out.println("Falha ao conectar"+"\n"+"VocÍ N√O EST¡ EM CONEX√O COM O BANCO DE DADOS"+e.toString());
-		JOptionPane.showMessageDialog(null, "erro  - falha ao conectar"+"\n"+"VocÍ N√O EST¡ EM CONEX√O COM O BANCO DE DADOS"+"\nFeche o programa e chame o tÈcnico"+e.toString());
+		System.out.println("Falha ao conectar"+"\n"+"Voc√™ N√ÉO EST√Å EM CONEX√ÉO COM O BANCO DE DADOS"+e.toString());
+		JOptionPane.showMessageDialog(null, "erro  - falha ao conectar"+"\n"+"Voc√™ N√ÉO EST√Å EM CONEX√ÉO COM O BANCO DE DADOS"+"\nFeche o programa e chame o t√©cnico"+e.toString());
 		e.printStackTrace();
 	}
 		return conexao;
@@ -34,7 +35,7 @@ private Connection conexao;
 	
 	/*
 	
-	 //quando testei essa conex„o, funcionou perfeitamente
+	 //quando testei essa conex√£o, funcionou perfeitamente
 	public static void main(String[]args) {
 		new ConexaoBD().conectar();
 		
